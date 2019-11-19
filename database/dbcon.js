9f6b961c0a15237528f01718d0fc5d8c85f3283e
Chinedu Emesue <de.magnificent1@gmail.com>
2019-11-19 16:04:49 +0100

    chore(cahnged database URL)

--- database/dbcon.js
+++ database/dbcon.js
@@ -1,7 +1,7 @@
 const {Client} = require('pg');
 
 //Connecting API to Postgres Database
-const conString = process.env.HEROKU_DB_URI || "postgres://devc:123456@localhost:5432/teamwork";
+const conString = process.env.DATABASE_URL || "postgres://devc:123456@localhost:5432/teamwork";
 const client = new Client(conString);
 client.connect();
 
