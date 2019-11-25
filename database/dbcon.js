c75d33e376a46fd250096a3964e88477e920b7eb
Chinedu Emesue <de.magnificent1@gmail.com>
2019-11-25 11:08:59 +0100

    chore(sending auth token)

--- database/dbcon.js
+++ database/dbcon.js
@@ -1,8 +1,8 @@
 const {Client} = require('pg');
 
 //Connecting API to Postgres Database
-//const conString = process.env.DATABASE_URL || process.env.LOCAL_DB_URL;
-const conString = process.env.LOCAL_DB_URL;
+const conString = process.env.DATABASE_URL || process.env.LOCAL_DB_URL;
+//const conString = process.env.LOCAL_DB_URL;
 const client = new Client(conString);
 client.connect();
 
