6938347584aec846a2bb1e8435d9b6dadab2efb0
Chinedu Emesue <de.magnificent1@gmail.com>
2019-11-25 16:26:13 +0100

    chores(setting up fetch api cors)

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(cors({
   optionsSuccessStatus: 200
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 //My app.js test script
 app.get('/', (req, res) => {
