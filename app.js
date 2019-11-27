e8b66948403c8199991d7d8d23b97b0c27c58db8
Chinedu Emesue <de.magnificent1@gmail.com>
2019-11-27 15:21:56 +0100

    bug(fix query errors)

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 //CORS - This allows access from different origin to my API
-app.use(cors({
-  origin: '*',
-  optionsSuccessStatus: 200
-}));
+// app.use(cors({
+//   origin: '*',
+//   optionsSuccessStatus: 200
+// }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
