3e680e0adc1395b2f9bbd7d7b993b7a9293f14d3
Chinedu Emesue <de.magnificent1@gmail.com>
2019-11-27 15:36:09 +0100

    bug(fix query errors)

--- app.js
+++ app.js
@@ -4,22 +4,19 @@ require('dotenv').config();
 const cors = require('cors')
 const app = express();
 
+//CORS - This allows access from different origin to my API
+app.use(cors());
+
 //Setup body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-//CORS - This allows access from different origin to my API
-// app.use(cors({
-//   origin: '*',
-//   optionsSuccessStatus: 200
-// }));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
 
 //My app.js test script
 app.get('/', (req, res) => {
